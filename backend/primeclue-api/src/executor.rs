51da329ce17fb04958236eabb9d6d3a74d836813
Łukasz Wojtów <lukasz.wojtow@gmail.com>
2020-12-19 00:01:19 +0100

    Remove debug from Executor

--- backend/primeclue-api/src/executor.rs
+++ backend/primeclue-api/src/executor.rs
@@ -108,10 +108,8 @@ impl Executor {
     pub(crate) fn status(&mut self, job_id: JobId) -> Option<Status> {
         let mut map = self.status_map.lock().unwrap();
         if let Some(status) = map.get(&job_id) {
-            dbg!(status);
             return if status.is_final() { map.remove(&job_id) } else { Some(status.clone()) };
         }
-        dbg!("None");
         None
     }
 
