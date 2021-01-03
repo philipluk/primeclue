958d3ae6d0e8d322daae7b16edb14dc6eaa3de33
Łukasz Wojtów <lukasz.wojtow@gmail.com>
2021-01-03 11:28:03 +0100

    Update deps, bump version to 0.3.0

--- backend/build.sh
+++ backend/build.sh
@@ -4,6 +4,6 @@ set -ex
 cargo lichking check --all
 cargo check --all-targets
 cargo clippy
-cargo test --release -q -- --nocapture
-USE_PREDICTABLE_RNG=1 cargo bench -q
+cargo test --release
+USE_PREDICTABLE_RNG=1 cargo bench
 
